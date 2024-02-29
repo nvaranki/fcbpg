d6a643a2ac68af6f9ee7f6c7bf1eb0a9a6456111
Nikolai Varankine <nvaranki@tut.by>
2024-02-29 21:05:07 +0300

    Biased matrices

--- src/main/java/com/varankin/bpg/Runner.java
+++ src/main/java/com/varankin/bpg/Runner.java
@@ -1,5 +1,6 @@
 package com.varankin.bpg;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Runner
@@ -7,26 +8,44 @@ public class Runner
 
     public static void main( String... args )
     {
-        Runner runner = new Runner();
+        Runner runner = new Runner( 4, 16, 2, true );
         float loss;
+        int n = 0;
         do
         {
-            loss = runner.epoch();
-            System.out.printf( "Epoch loss: %5.3f%n", loss ); //TODO
+            loss = runner.epoch( 2 );
+            System.out.printf( "Epoch #%d loss: %5.3f%n", n++, loss ); //TODO
         }
-        while( loss > 0.001F ); //TODO
+        while( Float.isFinite( loss ) && loss > 0.050F ); //TODO
         System.out.println( "Finished." );
     }
 
-    private record IO( float[] inp, float[] out ) {}
+    private record IO( float[] inp, float[] out )
+    {
+        float[] inp( int s )
+        {
+            if( s == 0 )
+            {
+                return inp;
+            }
+            else
+            {
+                float[] ext = Arrays.copyOf( inp, inp.length + s );
+                Arrays.fill( ext, inp.length, ext.length, 1F );
+                return ext;
+            }
+        }
+    }
 
     private final float[][] w, q;
     private final IO[] io;
+    private final int sb;
 
-    public Runner()
+    public Runner( int sx, int sh, int sy, boolean bias )
     {
-        w = new float[4][3];
-        q = new float[3][2];
+        sb = bias ? 1 : 0;
+        w = new float[sx+sb][sh];
+        q = new float[sh+sb][sy];
         Random rnd = new Random( 12345L );
         for( float[] a : w )
             for( int i = 0; i < a.length; i++ )
@@ -53,17 +72,17 @@ public class Runner
         }
     }
 
-    private float epoch()
+    private float epoch( int sy )
     {
-        float[] y = new float[2];
+        float[] y = new float[sy+sb];
         double loss = 0F;
         for( IO p : io )
         {
-            float[] x = p.inp();
+            float[] x = p.inp( sb );
             float[] h = infer( x, y );
 
             float[] t = p.out();
-            float[] e = new float[y.length];
+            float[] e = new float[y.length-sb];
             for( int i = 0; i < e.length; i++ )
             {
                 e[i] = t[i] - y[i];
@@ -74,38 +93,38 @@ public class Runner
         return (float) Math.sqrt( loss / io.length );
     }
 
-    private static float[][] backward1( float[] x, float[][] w, float[][] q, float[] e )
+    private float[][] backward1( float[] x, float[][] w, float[][] q, float[] e )
     {
-        float[][] dw = new float[x.length][q.length];
+        float[][] dw = new float[w.length][q.length-sb];
         for( int ix = 0; ix < x.length; ix++ )
-            for( int iq = 0; iq < q.length; iq++ )
+            for( int iq = 0; iq < q.length-sb; iq++ )
             {
                 float s = 0F;
                 for( int ie = 0; ie < e.length; ie++ )
                     s += e[ie] / q[iq][ie];
-                dw[ix][iq] = s / x[ix] / e.length; //TODO Float.NaN Float.*_INFINITY
+                dw[ix][iq] = s / x[ix] / x.length; //TODO Float.NaN Float.*_INFINITY
                 if( ! Float.isFinite( dw[ix][iq] ) )
                     dw[ix][iq] = 0F;
             }
         return dw;
     }
 
-    private static float[][] backward0( float[] x, float[][] w, float[] e )
+    private float[][] backward0( float[] x, float[][] w, float[] e )
     {
-        float[][] dw = new float[x.length][e.length];
+        float[][] dw = new float[w.length][e.length];
         for( int ix = 0; ix < x.length; ix++ )
             for( int ie = 0; ie < e.length; ie++ )
             {
-                dw[ix][ie] = e[ie] / x[ix] / e.length; //TODO Float.NaN Float.*_INFINITY
+                dw[ix][ie] = e[ie] / x[ix] / x.length; //TODO Float.NaN Float.*_INFINITY
                 if( ! Float.isFinite( dw[ix][ie] ) )
                     dw[ix][ie] = 0F;
             }
         return dw;
     }
 
-    private static void forward( float[] a, float[][] w, float[] r )
+    private void forward( float[] a, float[][] w, float[] r )
     {
-        for( int ir = 0; ir < r.length; ir++ )
+        for( int ir = 0; ir < r.length - sb; ir++ )
         {
             float v = 0F;
             for( int ia = 0; ia < a.length; ia++ )
@@ -132,6 +151,7 @@ public class Runner
     private float[] infer( float[] x, float[] y )
     {
         float[] h = new float[q.length];
+        Arrays.fill( h, q.length-sb, q.length, 1F );
         forward( x, w, h );
         forward( h, q, y );
         return h;
